bc3f4e0e335d1ecc06dfe007cac26807aab2df8b
Oliver Tepper <oltepper@gmail.com>
2022-07-22 10:00:41 +0200

    2

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
- 
+from . import forms
 
 # Create your views here.
+
+
 def blog(request):
     # query the db to return all project objects
-    #blogs = blog.objects.all()
+    # blogs = blog.objects.all()
     return render(request, 'blog_folder/index.html')
